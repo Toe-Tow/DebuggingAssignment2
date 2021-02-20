af1175c5fec6566cff928d78d560e0f8081db455
Toe-Tow <62809444+Toe-Tow@users.noreply.github.com>
2021-02-19 22:09:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
-import Sidebar from './components/Sidebar.js';
+import React from 'react';
 
-const s = new Sidebar({
-  menu: [
-    'Albert Eggstein',
-    'Attila the Hen',
-    'Attila the Hen',
-    'Dixie Chick',
-    'Gregory Peck',
-    'Mary Poopins'
-  ]
-});
+class Example extends React.Component {
 
-/* 
-  TODO: Use the render() method of Sidebar to show HTML response on console. Results should be wrapped in <ul></ul>
-  
-  The final result should look like the following:
-  <ul><li>Albert Eggstein</li><li>Attila the Hen</li><li>Attila the Hen</li><li>Dixie Chick</li><li>Gregory Peck</li><li>Mary Poopins</li></ul>
-*/
+render(){
+      <p>My name is Torin Krishnarao</p>;
+     } 
+}
 
-//Here I used the console.log the use the render method so I could display the Sidebar content as an unordered list
-console.log(`<ul>${s.render()}</ul>`);
+export default Example;  
